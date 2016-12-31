07a2535bcadcd98718eb762b0774c1481af798cc
IgorPelevanyuk <gavelock@gmail.com>
2016-12-31 20:52:54 +0300

    UPDATE: First set of tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
-  // your code will go here
-
+    $.ajax({
+    url: 'https://www.codeschool.com/users/Gavelock.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var i=0; i<response.courses.completed.length; i++) {
+         var current = response.courses.completed[i];
+         console.log(current);
+         $('#badges').append('<div></div>')
+         $('#badges > div').last().append('<h3>' + current.title + '</h3>');
+         $('#badges > div').last().append('<img src="' + current.badge + '"/>');
+         $('#badges > div').last().append('<a href="' + current.url + '" target="_blank" class="btn btn-primary"></a>');
+         
+      }
+      $('#badges > div').addClass('course');
+    }
+  });
 });
